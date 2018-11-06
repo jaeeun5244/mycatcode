9aff34ab850996d9ca3136ad76f091d480808df2
Jaeeun Lee <eun5244@naver.com>
2018-11-06 20:16:59 +0900

    첫번째 커밋

---
+++ app/src/main/java/kr/hs/emirim/lg/mycatcode/ResultActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.lg.mycatcode;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
